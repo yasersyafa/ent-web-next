71e4878131949a446dfe24ed6d8fe1d828706c3f
yasersyafa <89734955+yasersyafa@users.noreply.github.com>
2025-07-02 20:14:00 +0700

    feat: add jumbotron ui for desktop device

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,49 @@
+import Navbar from "@/components/partials/navbar";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="bg-[url('/pattern.svg')] bg-no-repeat object-cover relative">
+      <Navbar />
+      {/* decorative elements */}
+      <div className="absolute left-20 bottom-80 hidden lg:block animate-bounce">
+        <div className="relative">
+          <div className="h-16 w-16 bg-[#134679]"></div>
+          <div className="absolute -bottom-2 -right-2 h-12 w-12 bg-teal-400"></div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+
+      <div className="absolute right-20 top-32 hidden lg:block animate-bounce">
+        <div className="relative">
+          <div className="h-12 w-12 bg-teal-400"></div>
+          <div className="absolute -bottom-2 -right-2 h-16 w-16 bg-[#134679]"></div>
+        </div>
+      </div>
+      {/* jumbotron */}
+      <div className="flex items-center flex-col justify-evenly min-h-screen w-full">
+        <div className="font-medium text-8xl max-w-4xl text-center">
+          <div className="group flex justify-center items-center gap-5">
+            <h1>EEPIS</h1>
+            <h1 className="group-hover:translate-x-10 transition-all ease-in-out">NEWS</h1>
+          </div>
+          <h1>AND</h1>
+          <div className="group flex justify-center items-center gap-5">
+            <h1 className="group-hover:-translate-x-10 transition-all ease-in-out">NETWORK</h1>
+            <h1>TEAM</h1>
+          </div>
+        </div>
+        {/* cta button */}
+        <Button size={'lg'} className="rounded-full text-xl relative inline-flex gap-5 justify-center items-center px-12 py-6 overflow-hidden bg-[#134679] group hover:bg-[#134179] hover:translate-y-1 transition-all ease-in-out" asChild>
+          <Link href={'/auth'} className="mx-auto">
+            Be Part of ENT
+            <span className="absolute right-4 translate-x-[10px] opacity-0 font-bold group-hover:translate-x-0 group-hover:block group-hover:opacity-100 transition-all ease-in-out duration-500">
+              <Image src={'/arrow-right.png'} alt="arrow" width={12} height={12} />
+            </span>
+          </Link>
+        </Button>
+      </div>
     </div>
   );
 }
